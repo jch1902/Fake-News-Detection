6795ef5e9bc7e85a511bbee68c061ed3fa183bd9
jch1902 <jasper.d39c@gmail.com>
2025-05-11 23:40:00 -0400

    Added pipeline for LSTM model

---
+++ src/LSTM.py
@@ -0,0 +1,122 @@
+import pandas as pd
+import keras
+import os
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Layer, Input, Embedding, LSTM, Dense, Dropout, Bidirectional
+from tensorflow.keras.saving import register_keras_serializable
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from import_data import import_data
+
+
+def split_data():
+    df = import_data()
+
+    X = df['text']  
+    y = df['fake_news_flag']  
+
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, stratify = y, random_state=42)
+
+    return X_train, X_test, y_train, y_test
+
+
+def tokenize_data(tokenizer, data, max_length = 300):
+    seq = tokenizer.texts_to_sequences(data)
+    pad = pad_sequences(seq, maxlen=max_length, padding='post')
+    return pad
+
+def create_tokenizer(vocab_size = 10000):
+    X_train, X_test, y_train, y_test = split_data()
+
+    tokenizer = Tokenizer(num_words=vocab_size, oov_token='<OOV>')
+    tokenizer.fit_on_texts(X_train)
+
+    return tokenizer
+    
+
+def train_model(model, early_stop, vocab_size = 10000, max_length = 300, embedding_dim = 128):
+    X_train, X_test, y_train, y_test = split_data()
+
+    tokenizer = create_tokenizer
+
+    X_train_seq = tokenizer.texts_to_sequences(X_train)
+    X_test_seq = tokenizer.texts_to_sequences(X_test)
+
+    X_train_pad = pad_sequences(X_train_seq, maxlen=max_length, padding='post')
+    X_test_pad = pad_sequences(X_test_seq, maxlen=max_length, padding='post')
+        
+    model.fit(
+            X_train_pad, y_train,
+            epochs=5,
+            batch_size=64,
+            validation_data=(X_test_pad, y_test),
+            callbacks=[early_stop]
+        )
+    
+    return model, tokenizer
+
+
+def fetch_model(vocab_size = 10000, max_length = 300, embedding_dim = 128):
+
+    @register_keras_serializable()
+    class Attention(Layer):
+        def call(self, inputs):
+            # inputs: (batch_size, time_steps, hidden_size)
+            score = tf.nn.tanh(inputs)
+            self.attention_weights = tf.nn.softmax(score, axis=1)
+            context_vector = self.attention_weights * inputs
+            context_vector = tf.reduce_sum(context_vector, axis=1)
+            return context_vector
+        def get_attention_weights(self):
+            return self.attention_weights
+
+    model = Sequential([
+        Embedding(input_dim=vocab_size, output_dim=embedding_dim, input_length=max_length),
+        Bidirectional(LSTM(64, return_sequences=True)),
+        Attention(),
+        Dropout(0.5),
+        Dense(64, activation='relu'),
+        Dropout(0.3),
+        Dense(1, activation='sigmoid')
+    ])
+
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    
+
+    # add early stopping
+    early_stop = EarlyStopping(
+        monitor='val_loss',      
+        patience=2,               
+        restore_best_weights=True 
+    )
+
+    model_path = '../LSTMModel.keras'
+
+    if os.path.exists(model_path):
+        model = keras.models.load_model(model_path)
+        print("Succesfully loaded model!")
+    else:
+        model = train_model(model, early_stop)
+        model.save('LSTMModel.keras')
+
+    model.summary()
+
+    return model
+
+def predict(data):
+    model = fetch_model()
+
+    tokenizer = create_tokenizer()
+    pad_data = tokenize_data(tokenizer, data)
+
+    prediction_probs = model.predict(pad_data)
+    predictions = (prediction_probs > 0.5).astype(int).flatten()
+    return predictions
+
+
+
+
---
+++ src/import_data.py
@@ -0,0 +1,85 @@
+import os
+import re
+import pandas as pd
+import numpy as np
+from sklearn.utils import shuffle
+from sklearn.decomposition import NMF
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+def clean_text(text):
+    # Remove patterns at beginning of text such as "WASHINGTON (Reuters) -"
+    cleaned_text = re.sub(r"^\S+(?:\s+\S+)*\s*\([^)]+\)\s*-", '', text)
+    cleaned_text = re.sub(r"\n", "", cleaned_text) # removing \n
+    cleaned_text = re.sub(r'\(.*?\)\s*-\s*', '', cleaned_text)
+    cleaned_text = re.sub(r'[^\w\s]', '', cleaned_text)
+    return cleaned_text
+
+
+def import_data():
+    output_path = '../cleaned_data/final_labeled_fulldata.csv'
+    if os.path.exists(output_path):
+        print(output_path, "already exists!")
+        return output_path
+    else:
+        # Combining true and false datasets
+        true_df = pd.read_csv('../True.csv')
+        false_df = pd.read_csv('../Fake.csv')
+        
+        true_df["fake_news_flag"] = 0
+
+        true_df['text'] = true_df['text'].apply(clean_text)
+        false_df["fake_news_flag"] = 1
+
+        combined_df = pd.concat([true_df, false_df], ignore_index=True)
+
+        big_df = pd.read_csv("../WELFake_Dataset.csv")
+        big_df["fake_news_flag"] = big_df["label"]
+
+        big_df.drop(columns=["Unnamed: 0","label"], inplace= True)
+        big_df = big_df.dropna()
+        big_df['text'] = big_df['text'].apply(clean_text)
+        big_df.head()
+
+        combined_df.drop(columns=['date','subject'], inplace= True)
+
+        combined_df = pd.concat([combined_df, big_df], ignore_index=True)
+
+        combined_df = shuffle(combined_df, random_state=42)
+
+
+
+        # Preprocessing and vectorizing the text data
+        vectorizer = TfidfVectorizer(max_features=1000, stop_words='english')
+        X = vectorizer.fit_transform(combined_df['text'])
+
+        num_topics = 9  
+        nmf_model = NMF(n_components=num_topics, random_state=42)
+        nmf_model.fit(X)
+
+        # Top words per topic
+        terms = vectorizer.get_feature_names_out()
+        topic_labels = []
+
+        for topic_idx, topic in enumerate(nmf_model.components_):
+            topic_words = [terms[i] for i in topic.argsort()[:-10 - 1:-1]] 
+            print(f"Topic #{topic_idx}: {' '.join(topic_words)}")
+
+
+        topic_label_map = {
+            0: 'U.S. Politics',
+            1: 'International Affairs',
+            2: 'U.S. Politics',
+            3: 'International Affairs',
+            4: 'U.S. Politics',
+            5: 'General News and Announcements',
+            6: 'International Affairs',
+            7: 'Legal and Political',
+            8: 'Social Issue'
+        }
+
+        topic_assignments = nmf_model.transform(X)
+        dominant_topic = topic_assignments.argmax(axis=1)
+
+        combined_df['dominant_topic'] = dominant_topic
+        combined_df['topic_label'] = combined_df['dominant_topic'].map(topic_label_map)
+        return combined_df
